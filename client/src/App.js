9a906aa460bd932557abace847a8c07e962ff17f
Ilya Soloveychik <dj.active.17@gmail.com>
2023-01-01 15:19:45 +0200

    add ui, axios for requests

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
 import {useEffect} from 'react';
 import './App.css';
+import doAPIRequest from './utils/doAPIRequest';
 
 function App() {
 
   useEffect(() => {
     // TEST API, it might be removed
-    fetch('http://localhost:8080/live').then(res => res.json()).then(res => {
-      console.log('API CONNECTION IS OK');
-    }).catch((e) => console.error('API CONNECTION FAILED, PLEASE CHECK SERVER APP AND TRY AGAIN'))
+    doAPIRequest('/live').then(res => console.log(res)).catch(e => console.error(e));
   }, []);
 
   return (
